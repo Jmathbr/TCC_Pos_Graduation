be6b307f5348a937eafdf6a3db87192cd64f0f44
Joao Matheus Bernardo Resende <joao.matheus92225@gmail.com>
2022-06-30 18:57:08 -0300

    creat save HSV values

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import imutils
 import cv2
+import json
 
 #trackbar callback fucntion to update HSV value
 # optional argument for trackbars
@@ -15,6 +16,16 @@ sh = 'S High'
 vl = 'V Low'
 vh = 'V High'
 
+try:
+    arquivo = open('SaveHSV.json', 'r+')
+except FileNotFoundError:
+    arquivo = open('SaveHSV.json', 'w+')
+    arquivo.writelines("{HSVLOWER:[0,0,0],HSVHIGH:[255,255,255]}")
+    arquivo.close()
+
+with open("SaveHSV.json", encoding='utf-8') as meu_json:
+    dados = json.load(meu_json)
+
 cap = cv2.VideoCapture(0)
 
 cv2.namedWindow(barsWindow, flags = cv2.WINDOW_AUTOSIZE)
@@ -26,16 +37,12 @@ cv2.createTrackbar(sh, barsWindow, 0, 255, nothing)
 cv2.createTrackbar(vl, barsWindow, 0, 255, nothing)
 cv2.createTrackbar(vh, barsWindow, 0, 255, nothing)
 
-cv2.setTrackbarPos(hl, barsWindow, 0)
-cv2.setTrackbarPos(hh, barsWindow, 179)
-cv2.setTrackbarPos(sl, barsWindow, 0)
-cv2.setTrackbarPos(sh, barsWindow, 255)
-cv2.setTrackbarPos(vl, barsWindow, 0)
-cv2.setTrackbarPos(vh, barsWindow, 255)
-
-# Variables for HSV values
-primeiro_frame = None
-threshold_binarizacao = 70
+cv2.setTrackbarPos(hl, barsWindow, dados['HSVLOWER'][0])
+cv2.setTrackbarPos(hh, barsWindow, dados['HSVHIGH'][0])
+cv2.setTrackbarPos(sl, barsWindow, dados['HSVLOWER'][1])
+cv2.setTrackbarPos(sh, barsWindow, dados['HSVHIGH'][1])
+cv2.setTrackbarPos(vl, barsWindow, dados['HSVLOWER'][2])
+cv2.setTrackbarPos(vh, barsWindow, dados['HSVHIGH'][2])
 
 
 
@@ -51,10 +58,8 @@ while True:
     val = cv2.getTrackbarPos(vl, barsWindow)
     vah = cv2.getTrackbarPos(vh, barsWindow)
 
-    # make array for final values
     HSVLOW = np.array([hul, sal, val])
     HSVHIGH = np.array([huh, sah, vah])
-
 
     kernel = np.ones((9, 9), np.uint8)
 
@@ -75,13 +80,26 @@ while True:
             cv2.circle(frame, (int(x), int(y)), int(radius),
                        (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+            cv2.putText(frame, "Contagem: {}".format(str(len(cnts))), (5, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (250, 0, 1), 2)
     
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
 
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0XFF == ord('s'):
+
+        arquivo = open('SaveHSV.json', 'w+')
+        arquivo.writelines('{\"HSVLOWER\":[' + str(HSVLOW[0]) + ',' + str(HSVLOW[1]) + ',' +
+        str(HSVLOW[2]) + '],\"HSVHIGH\":[' + str(HSVHIGH[0]) + ',' + str(HSVHIGH[1]) + ',' + 
+        str(HSVHIGH[2]) + ']}')
+        arquivo.close()
+
         break
 
-# clean up our resources
+    if cv2.waitKey(1) & 0XFF == 27:
+        break
+
+
 cap.release()
 cv2.destroyAllWindows()
